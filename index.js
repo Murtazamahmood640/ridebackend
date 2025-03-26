986e4fd09aa86a194b961853e02cbb368176ebc8
Murtaza Mehmood <murtazamahmood640@gmail.com>
2025-03-26 16:20:12 +0500

    hello

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv')
-const app = express();
-<<<<<<< HEAD
+const dotenv = require('dotenv');
 const cors = require("cors");
-dotenv.config()
-require("./conn/conn")
-=======
+require("./conn/conn");
+const app = express();
 
 // Import the cron job from utils
 require("./utils/cronScheduler");  // This will execute the cron job when server starts
@@ -22,34 +19,13 @@ const trips = require('./routes/Trips/trips');
 const userDriver = require('./routes/UserManagment/userDriver');
 const logsRouter = require('./routes/Logs/LogController');
 
->>>>>>> 686b591 (check)
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-const auth = require("./routes/UserManagment/auth")
-const userPassenger = require('./routes/UserManagment/userPassenger')
-const dispatcher = require('./routes/UserManagment/dispatacher')
-const accountant = require('./routes/UserManagment/accountant')
-const vehicle = require('./routes/VehicleManagment/Vehicle')
-const allDriver = require('./routes/Driver/allDrivers')
-const driverRequest = require('./routes/Driver/driverRequest')
-const trips = require('./routes/Trips/trips')
-const userDriver = require('./routes/UserManagment/userDriver')
-const logsRouter  = require('./routes/Logs/LogController')
-app.use(express.json())
-app.get('/' , (req ,res) =>{
-    res.send("Hello") 
+app.use(express.json());
+
+// API routes
+app.get('/', (req, res) => {
+  res.send("Hello");
 });
-<<<<<<< HEAD
-app.use("/api/viaRide", auth)
-app.use("/api/ViaRide", userPassenger)
-app.use("/api/ViaRide", userDriver)
-app.use("/api/viaRide", dispatcher)
-app.use("/api/viaRide", accountant)
-app.use("/api/viaRide", vehicle)
-app.use("/api/viaRide", allDriver)
-app.use("/api/viaRide", driverRequest)
-app.use("/api/viaRide", trips)
-app.use("/api/viaRide", logsRouter )
-=======
 app.use("/api/viaRide", auth);
 app.use("/api/ViaRide", userPassenger);
 app.use("/api/ViaRide", userDriver);
@@ -60,9 +36,8 @@ app.use("/api/viaRide", allDriver);
 app.use("/api/viaRide", driverRequest);
 app.use("/api/viaRide", trips);
 app.use("/api/viaRide", logsRouter);
->>>>>>> 686b591 (check)
 
 const PORT = process.env.PORT || 1000;
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`) 
-}); 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
